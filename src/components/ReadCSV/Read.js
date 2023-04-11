81cd8737ef02a51a42ccd8f8aeb7cec84b088221
Carmelo Hernàndez <hernandc150@gmail.com>
2023-04-11 14:33:01 -0600

    se agrega funcion para leer archivos CSV

---
+++ src/components/ReadCSV/Read.js
@@ -0,0 +1,42 @@
+
+function Read() {
+
+/*Funciòn para leer archivo csv*/
+    const readFile = ( e ) => {
+  
+      const file = e.target.files[0];
+      if ( !file ) return;
+  
+      const fileReader = new FileReader();
+  
+      fileReader.readAsText( file );
+  
+      fileReader.onload = () => {
+        console.log( fileReader.result );
+        /*Recorremos el a.result para obtener la información*/
+        
+      }
+  
+      fileReader.onerror = () => {
+        console.log( fileReader.error );
+      }
+  
+    }
+
+    return (
+      <div className="contenedor-principal-read">
+       
+          
+          <h1>Leer archivos de texto </h1>
+  
+          <br />
+          <input className="btn-general-upload"
+            type="file"
+            multiple={ false }
+            onChange={ readFile }
+          />
+  
+     
+      </div>
+    );
+  }
