bd0736a8409967a6ac05787847188d54ce445623
Daniel Ortiz <albertodanielortiz04@gmail.com>
2023-04-12 16:42:28 -0600

    Se termina componente de ahorcado

--- src/components/Hang/Word.js
+++ src/components/Hang/Word.js
@@ -1,7 +1,12 @@
 function Word(props) {
   return (
-    <span className="wordSecret">
-      {props.letter}
-    </span>
+    <React.Fragment>
+      {props.letter === " "
+       ? (
+        <div className="wordSecret" style={{border: 'none'}}><span></span></div>
+      ) : (
+        <div className="wordSecret"><span>{props.letter}</span></div>
+      )}
+    </React.Fragment>
   );
 }
---
+++ src/components/Hang/data.js
@@ -0,0 +1,20 @@
+const listQuestions = [
+  {
+    id: 1,
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean accumsan est at nunc fermentum ultrices.",
+    answer: "moodle",
+  },
+  {
+    id: 2,
+    question:
+      "Aenean accumsan est at nunc fermentum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    answer: "subitus mx",
+  },
+  {
+    id: 3,
+    question:
+      "Est at nunc fermentum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    answer: "daniel",
+  }
+];
