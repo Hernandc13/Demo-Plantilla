477b1f1e9c571b88d7af7c74f9572962047e5dbb
Carmelo Hernàndez <hernandc150@gmail.com>
2023-04-17 09:56:02 -0600

    nuevos componentes card, slider

---
+++ src/components/Card/Card.js
@@ -0,0 +1,49 @@
+function Card() {
+    return (
+        <div className="contenedor-principal">
+            <h1>Card</h1>
+
+            <section className="container">
+                <div className="card">
+                    <div className="image">
+                        <img src="https://www.subitus.com/wp-content/uploads/2022/09/Santi_Clemente_Subitus-150x150.jpeg" alt="" />
+                    </div>
+                    <h2>Someone Name</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src="https://www.subitus.com/wp-content/uploads/2020/05/Fabian_Gutierrez_Subitus_Certificado_Moodle-150x150.jpg" alt="" />
+                    </div>
+                    <h2>Someone Name</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src="https://www.subitus.com/wp-content/uploads/2022/09/Santi_Clemente_Subitus-150x150.jpeg" alt="" />
+                    </div>
+                    <h2>Someone Name</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                </div>
+                <div className="card">
+                    <div className="image">
+                        <img src="https://www.subitus.com/wp-content/uploads/2020/05/Fabian_Gutierrez_Subitus_Certificado_Moodle-150x150.jpg" alt="" />
+                    </div>
+                    <h2>Someone Name</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                </div>
+            </section>
+
+        </div>
+
+    );
+}
+
+
+
+
+
+
+
+
+
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,25 @@
+function Slider() {
+    return (
+        <div className="contenedor-principal">
+            <h1>Slider</h1>
+            <section className="wrapper">
+                <i className="fa-solid fa-arrow-left button" id="prev"></i>
+                <div className="image-container">
+                    <div className="carousel">
+                        <img src="https://www.subitus.com/wp-content/uploads/2019/07/blond-cellphone-coffee-1076814.jpg" alt="" />
+                        <img src="https://www.subitus.com/wp-content/uploads/2020/04/e-learning_servicios.jpeg" alt="" />
+                        <img src="https://www.biossmann.com/statics/images/recursos_humanos1.jpg" alt="" />
+                        <img src="https://www.biossmann.com/statics/images/proposito.jpg" alt="" />
+                    </div>
+                    <i className="fa-solid fa-arrow-right button" id="next"></i>
+                </div>
+            </section>
+        </div>
+
+    );
+}
+
+
+
+
+
