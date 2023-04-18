eaa454efb8a3244db782abcd67385b1e23e6d768
Carmelo Hernàndez <hernandc150@gmail.com>
2023-04-17 20:28:02 -0600

    se integran componente scroll y cards

---
+++ src/components/Card_Animado/CardAnimado.js
@@ -0,0 +1,29 @@
+function CardAnimado() {
+    return (
+      <div className="contenedorPrincipal">
+    <h1>DESARROLLO WEB</h1>
+    <div className="container">
+        <div className="card2">
+            <div className="title">
+                <h1>FRONTEND </h1>
+            </div>
+            <a href="" className="btn2">INTRO</a>
+
+        </div>
+        <div className="card2">
+            <div className="title">
+                <h1>BACKEND </h1> 
+            </div>
+            <a href="" className="btn2">INTRO</a>
+        </div>
+        <div className="card2">
+            <div className="title">
+                <h1>FULLSTACK </h1>
+            </div>
+            <a href="" className="btn2">INTRO</a>
+        </div>
+    </div>
+      </div>
+    );
+  }
+  
