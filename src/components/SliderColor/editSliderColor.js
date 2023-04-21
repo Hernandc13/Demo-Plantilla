f2eca88c11b07dae6fa52c5d788a78ba0cad3427
Daniel Ortiz <albertodanielortiz04@gmail.com>
2023-04-21 13:52:58 -0600

    se hacen editables sliders

---
+++ src/components/SliderColor/editSliderColor.js
@@ -0,0 +1,38 @@
+const imagesSliderTwo = [
+    {
+      id: 1,
+      url: "https://images.unsplash.com/photo-1560250097-0b93528c311a?fit=crop&amp;h=720&amp;q=80",
+      name: "Brandon Murray",
+      title: "Central Functionality VP"
+    },
+    {
+      id: 2,
+      url: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?fit=crop&amp;h=720&amp;q=80",
+      name: "Alicia Chevalier",
+      title: "Corporate Usability Analyst",
+    },
+    {
+      id: 3,
+      url: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?fit=crop&amp;h=720&amp;q=80",
+      name: "Nate Boucher",
+      title: "Customer Impact Officer",
+    },
+    {
+      id: 4,
+      url: "https://images.unsplash.com/photo-1614204424926-196a80bf0be8?fit=crop&amp;h=720&amp;q=80",
+      name: "Leah Harris",
+      title: "Designer and Bandit",
+    },
+    {
+      id: 5,
+      url: "https://images.unsplash.com/photo-1536766768598-e09213fdcf22?fit=crop&amp;h=720&amp;q=80",
+      name: "Angelina Laurent",
+      title: "Oracle for Inspiration",
+    },
+    {
+      id: 6,
+      url: "https://images.unsplash.com/photo-1553514029-1318c9127859?fit=crop&amp;h=720&amp;q=80",
+      name: "Candice Marchand",
+      title: "Mindful Realist of Motion Laws"
+    },
+  ];
